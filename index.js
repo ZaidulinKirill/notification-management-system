7788a88f233bbab7332c40518ea21a162e24f471
Zaidulin Kirill <zaidulinkirill@gmail.com>
2019-10-03 23:43:43 +0300

    Add context

--- index.js
+++ index.js
@@ -2,27 +2,32 @@ import smsSender from './shared/smsSender'
 import emailSender from './shared/emailSender'
 import telegramSender from './shared/telegramSender'
 
-export default ({events}) => {
+export default ({ events }) => {
   return (value, oldValue) => {
-    await Promise.all(events
+    const context = await event.preSend ? event.preSend(value, oldValue) || {} : {}
+
+    return Promise.all(events
       .filter(event => event.condition(value, oldValue))
-      .map(event => {
+      .map(async event => {
         return (event.channels || []).map(channel => {
           switch(channel.type) {
             case 'sms': {
-              return smsSender(channel.message(value), channel.recipients)
+              return smsSender(
+                channel.message({ item: value, ...context }), 
+                channel.recipients({ item: value, ...context })
+              )
             }
             case 'email': {
               return emailSender({
-                subject: channel.subject, 
-                emails: channel.recipients,
-                html: channel.message(value),
+                subject: channel.subject({ item: value, ...context }), 
+                emails: channel.recipients({ item: value, ...context }),
+                html: channel.message({ item: value, ...context }),
               })
             }
             case 'telegram': {
               return telegramSender({
-                ids: channel.recipients,
-                message: channel.message(value),
+                ids: channel.recipients({ item: value, ...context }),
+                message: channel.message({ item: value, ...context }),
               })
             }
             default: {
