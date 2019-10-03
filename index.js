27abefe8432b2702ef45fcb6b4822789f4d11c62
Zaidulin Kirill <zaidulinkirill@gmail.com>
2019-10-03 23:44:35 +0300

    Revert

--- index.js
+++ index.js
@@ -3,41 +3,39 @@ import emailSender from './shared/emailSender'
 import telegramSender from './shared/telegramSender'
 
 export default ({ events }) => {
-  return (value, oldValue) => {
-    const context = await event.preSend ? event.preSend(value, oldValue) || {} : {}
+  return (value, oldValue) => Promise.all(events
+    .filter(event => event.condition(value, oldValue))
+    .map(async event => {
+      event.context = await event.preSend ? event.preSend(value, oldValue) || {} : {}
 
-    return Promise.all(events
-      .filter(event => event.condition(value, oldValue))
-      .map(async event => {
-        return (event.channels || []).map(channel => {
-          switch(channel.type) {
-            case 'sms': {
-              return smsSender(
-                channel.message({ item: value, ...context }), 
-                channel.recipients({ item: value, ...context })
-              )
-            }
-            case 'email': {
-              return emailSender({
-                subject: channel.subject({ item: value, ...context }), 
-                emails: channel.recipients({ item: value, ...context }),
-                html: channel.message({ item: value, ...context }),
-              })
-            }
-            case 'telegram': {
-              return telegramSender({
-                ids: channel.recipients({ item: value, ...context }),
-                message: channel.message({ item: value, ...context }),
-              })
-            }
-            default: {
-              throw new Error('Not implemented')
-            }
+      return (event.channels || []).map(channel => {
+        switch(channel.type) {
+          case 'sms': {
+            return smsSender(
+              channel.message({ item: value, ...event.context }), 
+              channel.recipients({ item: value, ...event.context })
+            )
           }
-        })
+          case 'email': {
+            return emailSender({
+              subject: channel.subject({ item: value, ...event.context }), 
+              emails: channel.recipients({ item: value, ...event.context }),
+              html: channel.message({ item: value, ...event.context }),
+            })
+          }
+          case 'telegram': {
+            return telegramSender({
+              ids: channel.recipients({ item: value, ...event.context }),
+              message: channel.message({ item: value, ...event.context }),
+            })
+          }
+          default: {
+            throw new Error('Not implemented')
+          }
+        }
       })
-    )
-  }
+    })
+  )
 }
 
 /**
